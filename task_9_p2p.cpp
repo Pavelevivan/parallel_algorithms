36fa24e08450aeb3d1116e72f18560b877fb4efe
Pavelyev Ivan Olegovich <pavelevivan@mail.ru>
2021-12-28 16:44:31 +0200

    task 9 update

---
+++ task_9_p2p.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <algorithm>
+#include "mpi.h"
+
+#define SIZE 1000000
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    double start, finish;
+    MPI_Status status;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int vec_base[SIZE];
+    const int batch_size = SIZE / size;
+    int vec_sum = 0;
+    int vec_sum_loc = 0;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        vec_base[i] = 1;
+    }
+
+    int strt = batch_size * rank;
+    int end = batch_size * (rank + 1);
+    if (rank == size - 1)
+        end = SIZE;
+
+    for (int i = strt; i < end; i++)
+    {
+        vec_sum_loc += vec_base[i];
+    }
+
+    start = MPI_Wtime();
+
+    for (int range = 2; range <= size; range *= 2)
+    {
+        if ((rank / range) * range + range / 2 == rank)
+        {
+            MPI_Send(&vec_sum_loc, 1, MPI_INT, (rank / range) * range, range, MPI_COMM_WORLD);
+        }
+        else if (rank % range == 0)
+        {
+            MPI_Recv(&vec_sum, 1, MPI_INT, (rank / range) * range + range / 2, range, MPI_COMM_WORLD, &status);
+            vec_sum_loc += vec_sum;
+        }
+    }
+
+    finish = MPI_Wtime();
+
+    if (rank == 0)
+    {
+        cout << "sum is: " << vec_sum_loc << endl;
+        cout << "The elapsed time for P2P process is " << finish - start << "seconds" << endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ task_9_reduce.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <algorithm>
+#include "mpi.h"
+
+#define SIZE 1000000
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    double start, finish;
+    MPI_Status status;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int vec_base[SIZE];
+    int vec_sum_loc = 0;
+    int vec_sum = 0;
+    int vec_summed_sum = 0;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        vec_base[i] = 1;
+    }
+
+    // divide vector into batches
+    const int batch_size = SIZE / size;
+
+    int strt = batch_size * rank;
+    int end = batch_size * (rank + 1);
+    if (rank == size - 1)
+        end = SIZE;
+
+    for (int i = strt; i < end; i++)
+    {
+        vec_sum_loc += vec_base[i];
+    }
+
+    start = MPI_Wtime();
+    MPI_Reduce(&vec_sum_loc, &vec_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    finish = MPI_Wtime();
+
+    if (rank == 0)
+    {
+        cout << "The final sum is " << vec_sum << endl;
+        cout << "The time is " << finish - start << " seconds" << endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
